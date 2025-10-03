dc66d4113ec42e8363145cd8ef8a841938f0c7ec
diegolsouzaa <diegolsouzaa@gmail.com>
2025-10-02 21:57:45 -0300

    Adicionar fixtures para json do login

--- tests/login.test.js
+++ tests/login.test.js
@@ -1,17 +1,20 @@
 import http from 'k6/http';
 import { sleep, check } from 'k6';
 
+//obter o conteudo do arquivo postLogin e convertendo para Json
+const postLogin = JSON.parse(open('../fixtures/postLogin.json')); 
+
 export const options = {
+  
   stages:[
     { duration:'10s', target: 10},
-    { duration:'20s', target: 10},
-    { duration:'10s', target: 30},
-    { duration:'20s', target: 30},
-    { duration:'20s', target: 0}
+    { duration:'20s', target: 20},
+    { duration:'10s', target: 0},
   ],
 
   thresholds: {
     http_req_duration: ['p(90)<3000', 'max<5000'],
+
     //a quantidade de requisições que falharam tem que ser de até 1%
      http_req_failed: ['rate<0.01']
   }
@@ -19,10 +22,18 @@ export const options = {
 
 export default function () {
     const url = 'http://localhost:3000/login';
-    const payload = JSON.stringify({
-        username: 'julio.lima',
-        senha: '123456',
-    });
+
+    //é possivel manipular os dados caso necessario
+    postLogin.username = "junior.souza"
+
+    // se quiser passar as informaçoes diretamente (não recomendado)    
+    // const payload = JSON.stringify({
+    //     username: 'julio.lima',
+    //     senha: '123456',
+    // });
+
+    console.log(postLogin)
+    const payload = JSON.stringify(postLogin);
 
     const params = {
         headers: {
